1da57d89df02df891d5068aff4d5f49eee8f3072
JonesWilde <davidgluttongk@gmail.com>
2020-03-11 17:35:11 -0700

    Initial commit

---
+++ src/test/java/come/gluttongk/exmaple/wxshop/WxshopApplicationTests.java
@@ -0,0 +1,13 @@
+package come.gluttongk.exmaple.wxshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
